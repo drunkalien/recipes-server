4bb1eb33916ed55ad2ef01bce9fcfe58bc3ed576
Jamshidkhuja Burikhujaev <jamshidxojab@gmail.com>
2023-05-24 10:08:52 +0500

    added connection to databse

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import recipeRouter from "./routes/recipeRouter";
 
 export const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+import mongoose from "mongoose";
+
 import { app } from "./app";
 
 app.listen(5000, () => console.log("app is running"));
+
+const DB = process.env.MONGO_URI || "mongodb://localhost:27017/recipe-app";
+
+mongoose
+  .connect(DB, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
